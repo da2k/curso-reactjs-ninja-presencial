0131b79dddb143c6440edce6ede2e4327c3e2fcc
Fernando Daciuk <f.daciuk@gmail.com>
2018-05-08 22:15:04 -0300

    CEP working

--- 2018-05-joinville/m01/busca-cep/src/App.js
+++ 2018-05-joinville/m01/busca-cep/src/App.js
@@ -1,21 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { PureComponent } from 'react'
+import axios from 'axios'
 
-class App extends Component {
-  render() {
+class BuscaCEP extends PureComponent {
+  state = {
+    code: '123123',
+    state: 'SP',
+    city: 'São Paulo',
+    district: 'Piratininga',
+    address: 'Rua Paula Rodrigues',
+    isFetching: false
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(e.target.cep.value)
+    const { value } = e.target.cep
+    if (!value) {
+      return
+    }
+    this.setState({ isFetching: true })
+    axios.get(`http://apps.widenet.com.br/busca-cep/api/cep/${value}.json`)
+      .then((result) => {
+        console.log(result.data)
+        this.setState({ isFetching: false })
+        this.setState(result.data)
+      })
+  }
+
+  render () {
+    const { code, state, city, district, address, isFetching } = this.state
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input id='cep' name='cep' />
+          <button type='submit'>Buscar CEP</button>
+        </form>
+        {isFetching && <div>Buscando CEP...</div>}
+        {!isFetching && (
+          <ul>
+            <li>CEP: {code}</li>
+            <li>Estado: {state}</li>
+            <li>Cidade: {city}</li>
+            <li>Bairro: {district}</li>
+            <li>Endereço: {address}</li>
+          </ul>
+        )}
       </div>
-    );
+
+    )
   }
 }
 
-export default App;
+export default BuscaCEP
