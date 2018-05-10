4cd6622ec9b56fe188ffd0dde972a0ef12bb176a
Fernando Daciuk <f.daciuk@gmail.com>
2018-05-10 17:07:17 -0300

    Update CEP app

--- 2018-05-joinville/m01/busca-cep/src/App.js
+++ 2018-05-joinville/m01/busca-cep/src/App.js
@@ -3,52 +3,77 @@ import axios from 'axios'
 
 class BuscaCEP extends PureComponent {
   state = {
-    code: '123123',
-    state: 'SP',
-    city: 'São Paulo',
-    district: 'Piratininga',
-    address: 'Rua Paula Rodrigues',
-    isFetching: false
+    code: '--',
+    state: '--',
+    city: '--',
+    district: '--',
+    address: '--',
+    isFetching: false,
+    errorMessage: null
   }
 
-  handleSubmit = (e) => {
+  handleSubmit = async (e) => {
     e.preventDefault()
     console.log(e.target.cep.value)
     const { value } = e.target.cep
-    if (!value) {
-      return
+    this.setState({ isFetching: true, errorMessage: null })
+
+    const [error, result] = await to(axios.get(`http://apps.widenet.com.br/busca-cep/api/cep/${value}.json`))
+
+    if (error) {
+      return console.log(error)
     }
-    this.setState({ isFetching: true })
-    axios.get(`http://apps.widenet.com.br/busca-cep/api/cep/${value}.json`)
-      .then((result) => {
-        console.log(result.data)
-        this.setState({ isFetching: false })
-        this.setState(result.data)
-      })
+
+    console.log('result:', result)
+    this.setState({ isFetching: false })
+    if (typeof result.data !== 'object') {
+      return this.setState({ errorMessage: result.data })
+    }
+    if (result.data.status === 0) {
+      return this.setState({ errorMessage: result.data.message })
+    }
+    this.setState(result.data)
   }
 
   render () {
-    const { code, state, city, district, address, isFetching } = this.state
+    const { isFetching } = this.state
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          <input id='cep' name='cep' />
+
+          <input
+            id='cep'
+            name='cep'
+            ref={this.ref}
+            defaultValue={this.state.cepValue}
+          />
+
           <button type='submit'>Buscar CEP</button>
         </form>
+
         {isFetching && <div>Buscando CEP...</div>}
-        {!isFetching && (
-          <ul>
-            <li>CEP: {code}</li>
-            <li>Estado: {state}</li>
-            <li>Cidade: {city}</li>
-            <li>Bairro: {district}</li>
-            <li>Endereço: {address}</li>
-          </ul>
-        )}
+        {!isFetching && <List {...this.state} />}
+
+        {this.state.errorMessage && <div>Erro: {this.state.errorMessage}</div>}
       </div>
 
     )
   }
+}
+
+const List = ({ code, state, city, district, address }) => (
+  <ul>
+    <li>CEP: {code}</li>
+    <li>Estado: {state}</li>
+    <li>Cidade: {city}</li>
+    <li>Bairro: {district}</li>
+    <li>Endereço: {address}</li>
+  </ul>
+
+)
+
+function to (promise) {
+  return promise.then((r) => [null, r]).catch((e) => [e])
 }
 
 export default BuscaCEP
